6689c91743c22c8b691cbd87c630bd23b0ce3bb4
redwanpp <smredwan.info@gmail.com>
2023-10-31 00:34:55 +0600

    start lawerMedia project

---
+++ LayerMedia/src/main/java/com/lawyer/LayerMediaApplication.java
@@ -0,0 +1,13 @@
+package com.lawyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayerMediaApplication.class, args);
+	}
+
+}

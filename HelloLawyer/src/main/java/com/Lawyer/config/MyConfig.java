07b48bd94cfe3dfe4c63720a00fdeaf0a79a9078
S. M. Redwan <107054369+redwanpp@users.noreply.github.com>
2023-11-13 12:05:15 +0600

    fixed email form error of server side validation

--- HelloLawyer/src/main/java/com/Lawyer/config/MyConfig.java
+++
@@ -1,5 +0,0 @@
-package com.Lawyer.config;
-
-public class MyConfig {
-
-}
--- HelloLawyer/src/main/java/com/Lawyer/dao/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Lawyer.dao;
-
-import com.Lawyer.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
